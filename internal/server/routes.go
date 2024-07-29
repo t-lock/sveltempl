b0d71a55a40b89d18edf2f182c3a431bbc7f4401
Tim Wheelock <tlock@armyofbees.com>
2024-07-30 02:14:52 +0900

    Drop previous experiments, reacreate original svelte pages in templ

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -12,32 +12,19 @@ import (
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
-	// mine
+	// creates a new JavaScript VM
 	iso := v8go.NewIsolate() // creates a new JavaScript VM
 
-	s.App.Get("/basic", adaptor.HTTPHandler(templ.Handler(web.Basic())))
-	s.App.Get("/code-only", adaptor.HTTPHandler(templ.Handler(web.CodeOnly("code <strong>only!</strong>"))))
+	// routes
+	s.App.Get("/", adaptor.HTTPHandler(templ.Handler(web.Home(iso))))
+	s.App.Get("/page", adaptor.HTTPHandler(templ.Handler(web.Page(iso))))
 
-	s.App.Get("/boxone", adaptor.HTTPHandler(templ.Handler(web.SvelTemplComponent("BoxOne", iso, nil))))
-	s.App.Get("/boxtwo", adaptor.HTTPHandler(templ.Handler(web.SvelTemplComponent("BoxTwo", iso, nil))))
-	s.App.Get("/boxthree", adaptor.HTTPHandler(templ.Handler(web.SvelTemplComponent("BoxThree", iso, nil))))
-	s.App.Get("/not-a-component", adaptor.HTTPHandler(templ.Handler(web.SvelTemplComponent("Nope", iso, nil))))
-	s.App.Get("/props", adaptor.HTTPHandler(templ.Handler(web.PropsExample(iso))))
-	s.App.Get("/", adaptor.HTTPHandler(templ.Handler(web.PageOne(iso))))
-
-	// blueprint
+	// blueprint assets
 	s.App.Use("/assets", filesystem.New(filesystem.Config{
 		Root:       http.FS(web.Files),
 		PathPrefix: "assets",
 		Browse:     false,
 	}))
-
-	s.App.Get("/web", adaptor.HTTPHandler(templ.Handler(web.HelloForm())))
-
-	s.App.Post("/hello", func(c *fiber.Ctx) error {
-		return web.HelloWebHandler(c)
-	})
-
 }
 
 func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
