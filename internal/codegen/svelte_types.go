a324c4e3cfd9d70e2d62d9b3bc3a9a873f1a205a
Tim Wheelock <tlock@armyofbees.com>
2024-08-04 14:17:39 +0900

    Restructure project to use sveltempl as a mockup library

---
+++ internal/codegen/svelte_types.go
@@ -0,0 +1,12 @@
+package codegen
+
+type components map[string]string
+
+// TODO make type-safe in a way that provides IDE completions
+// TODO code-gen this from Vite
+var ComponentMap = components{
+	"BoxOne":      "/assets/svelte/BoxOne-DhYVVjlR.js",
+	"BoxTwo":      "/assets/svelte/BoxTwo-DQX7MQTX.js",
+	"BoxThree":    "/assets/svelte/BoxThree-CRDtwcGO.js",
+	"TrelloClone": "/assets/svelte/TrelloClone-C7zW6e2i.js",
+}
--- internal/sveltempl/v8.go
+++ internal/sveltempl/v8.go
@@ -1,4 +1,4 @@
-package v8
+package sveltempl
 
 import (
 	"context"
